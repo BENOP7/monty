fde8ffa5f8e502f994be5c55177e324e99d9307a
benop7 <benjaminpetegnong@gmail.com>
2024-01-22 03:14:53 +0100

    task 6

--- monty.h
+++ monty.h
@@ -65,6 +65,7 @@ void pint(stack_t **stack, unsigned int line_number);
 
 void nop(stack_t **stack, unsigned int line_number);
 void add(stack_t **, unsigned int);
+void sub(stack_t **, unsigned int);
 int interprete(char **, unsigned int);
 void allocate_memory(void);
 void free_stack(void);
