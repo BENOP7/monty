39f2336907c705f2b295f6043bf4f1dff7eb6cdd
Obbie254 <obbienjehia@gmail.com>
2024-01-23 23:12:08 +0300

    betty error sorted

---
+++ rot1.c
@@ -0,0 +1,25 @@
+/**
+ * rotr - opcode; rotates the top element
+ * @stack: pointer to stack
+ * @line_number: instruction line number
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+	(void) line_number;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return;
+
+	tmp = *stack;
+	while (tmp->next)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	tmp->prev->next = NULL;
+	tmp->prev = NULL;
+
+	(*stack)->prev = tmp;
+	*stack = tmp;
+
+}
